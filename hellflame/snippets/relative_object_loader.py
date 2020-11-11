295a95d6c548d2f5e8528ad42803e4b4eb472749
yuuho <y.horiuchi@suou.waseda.jp>
2020-11-11 11:54:39 +0900

    save

--- hellflame/snippets/relative_object_loader.py
+++ hellflame/snippets/relative_object_loader.py
@@ -4,7 +4,7 @@ import inspect
 
 def load_object(path, key):
     mod_file = str(( Path( inspect.stack()[1].filename ).parent / path ).resolve())
-    mod_name = '_mod_'+(hash(path)>=0 and 'p' or 'n')+str(abs(hash(path)))
+    mod_name = '_mod_'+str(hash(path)).replace('-','n')
     mod = SourceFileLoader( mod_name, mod_file ).load_module()
     return getattr(mod,key)
 
